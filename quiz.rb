8898521583b38cae8b940d89e18ed9b35ee75885
Grant Hutchins <nertzy@gmail.com>
2013-01-08 11:46:47 -0500

    multiple choice question

--- quiz.rb
+++ quiz.rb
@@ -5,14 +5,42 @@ QUIZ = {
 
   question_2: {
     prompt: "What is your favorite color?"
+  },
+
+  question_3: {
+    prompt: "What is your favorite integer between 1 and 5?",
+    choices: %w[1 2 3 4 5]
   }
 }
 
 answers = {}
 
-QUIZ.each do |name, question_hash|
+def simple_answer(question_hash)
   print question_hash[:prompt], " "
-  answers[name] = gets.chomp
+  gets.chomp
+end
+
+def multiple_choice_answer(question_hash)
+  choices = question_hash[:choices]
+  answer = nil
+
+  until choices.include?(answer)
+    print question_hash[:prompt], " "
+    answer = gets.chomp
+    puts "You must pick an answer from #{choices}" unless choices.include?(answer)
+  end
+
+  answer
+end
+
+QUIZ.each do |name, question_hash|
+  answer = if question_hash[:choices]
+    multiple_choice_answer(question_hash)
+  else
+    simple_answer(question_hash)
+  end
+
+  answers[name] = answer
 end
 
 p answers

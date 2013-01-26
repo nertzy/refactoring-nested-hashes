556b31f970af40cf9fda275e54a3327ab9b4ee31
Grant Hutchins <nertzy@gmail.com>
2013-01-26 16:28:54 -0500

    What I did in the talk.

--- quiz.rb
+++ quiz.rb
@@ -1,17 +1,36 @@
-QUIZ = {
-  question_1: {
+require "rubygems"
+require "active_support/all"
+
+class Quiz < Hash
+
+end
+
+class QuestionHash
+  delegate :[], to: :@hash
+
+  def initialize(hash)
+    @hash = hash
+  end
+
+  def has_choices?
+    @hash.has_key?(:choices)
+  end
+end
+
+QUIZ = Quiz.new.replace( {
+  question_1: QuestionHash.new(
     prompt: "What is your name?"
-  },
+  ),
 
-  question_2: {
+  question_2: QuestionHash.new(
     prompt: "What is your favorite color?"
-  },
+  ),
 
-  question_3: {
+  question_3: QuestionHash.new(
     prompt: "What is your favorite integer between 1 and 5?",
     choices: %w[1 2 3 4 5]
-  }
-}
+  )
+} )
 
 answers = {}
 
@@ -34,7 +53,7 @@ def multiple_choice_answer(question_hash)
 end
 
 QUIZ.each do |name, question_hash|
-  answer = if question_hash[:choices]
+  answer = if question_hash.has_choices?
     multiple_choice_answer(question_hash)
   else
     simple_answer(question_hash)
